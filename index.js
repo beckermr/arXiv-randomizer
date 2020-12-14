0525b798e67c6331366eadcb8b11e971f704795b
Matthew R. Becker <becker.mr@gmail.com>
2020-12-14 13:11:35 -0600

    REF rewrite in javascript

---
+++ index.js
@@ -0,0 +1,190 @@
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+// get time in EST
+var now = dayjs.utc().tz('America/New_York')
+// 1 = monday, ..., 7 = sunday
+const day = (now.day() + 6) % 7 + 1
+// fraction hour to the nearest minute
+const hr = now.hour() + now.minute() / 60.0
+console.log('now: ' + now.format())
+console.log('day: ' + day)
+console.log('hour: ' + hr)
+
+// figure out if it is "today"
+var today = false
+if (hr >= 20.5) {
+  today = true
+}
+console.log('today: ' + today)
+
+now = now.hour(0).minute(0).second(0)
+
+// arXiv "logic" for sending out new articles
+// see this (http://arxiv.org/help/submit) and draw a picture
+var de
+var ds
+if (day === 7) {
+  // sunday
+  if (today) {
+    // night, so seeing stuff submitted from thurs 16:00 to fri 16:00
+    de = now.add(16 - 2 * 24, 'h')
+    ds = de.add(-24, 'h')
+  } else {
+    // day, so seeing stuff from wed 16:00 to thurs 16:00
+    de = now.add(16 - 3 * 24, 'h')
+    ds = de.add(-24, 'h')
+  }
+} else if (day === 1) {
+  // monday
+  if (today) {
+    // monday night, see stuff from fri 16:00 to mon 16:00
+    de = now.add(16, 'h')
+    ds = de.add(-24 * 3, 'h')
+  } else {
+    // monday day, seeing stuff from thurs 16:00 to fri 1600
+    de = now.add(16 - 3 * 24, 'h')
+    ds = de.add(-24, 'h')
+  }
+} else if (day === 2) {
+  // tuesday
+  if (today) {
+    // tuesday night is normal (monday 16 to tues 16)
+    de = now.add(16, 'h')
+    ds = de.add(-24, 'h')
+  } else {
+    // tues during day is fri 16 to mon 16
+    de = now.add(-8, 'h')
+    ds = de.add(-24 * 3, 'h')
+  }
+} else if (day === 5) {
+  // friday - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
+  de = now.add(16 - 24, 'h')
+  ds = de.add(-24, 'h')
+} else if (day === 6) {
+  // sat - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
+  de = now.add(16 - 24 * 2, 'h')
+  ds = de.add(24, 'h')
+} else {
+  // all other days are "normal"
+  if (today) {
+    de = now.add(16, 'h')
+  } else {
+    de = now.add(-8, 'h')
+  }
+  ds = de.add(-24, 'h')
+}
+console.log('ds: ' + ds)
+console.log('de: ' + de)
+
+// https://stackoverflow.com/questions/1787322/htmlspecialchars-equivalent-in-javascript
+function escapeHtml (text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  }
+
+  return text.replace(/[&<>"']/g, function (m) { return map[m] })
+}
+
+function htmlDecode (input) {
+  var e = document.createElement('textarea')
+  e.innerHTML = input
+  // handle case of empty input
+  return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue
+}
+
+const feedURL = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fexport.arxiv.org%2Frss%2Fastro-ph&api_key=io4ysvap45khsblw1vpswkdz0fym8wusye2gumeg'
+
+function loadArXivJSON () {
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', feedURL, true)
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      // Required use of an anonymous callback as .open will NOT
+      // return a value but simply returns undefined in asynchronous mode
+      parseFeed(xobj.responseText)
+    }
+  }
+  xobj.send(null)
+}
+
+function parseFeed (feedText) {
+  const feed = JSON.parse(feedText)
+
+  console.log(feed.items.length)
+  var container = document.getElementById('feed')
+  var tdiv = document.createElement('div')
+  tdiv.className = 'timestamp'
+  tdiv.innerHTML = (
+    'Showing ' +
+    feed.items.length +
+    ' posts from ' +
+    ds.format('ddd, D MMM YYYY HH:mm:ss') +
+    ' EDT to ' +
+    de.format('ddd, D MMM YYYY HH:mm:ss') +
+    ' EDT.'
+  )
+  container.appendChild(tdiv)
+
+  var entry
+  var i
+  var entries = []
+  for (i = 0; i < feed.items.length; i++) {
+    entry = feed.items[i]
+    if (!entry.title.includes('UPDATED')) {
+      entries.push(entry)
+    }
+  }
+  entries = entries.sort(() => Math.random() - 0.5)
+
+  var row, col, par, title, num, div
+  for (i = 0; i < entries.length; i++) {
+    entry = entries[i]
+
+    if (i % 2 === 0) {
+      row = document.createElement('div')
+      row.className = 'row'
+      container.appendChild(row)
+    }
+
+    col = document.createElement('div')
+    col.className = 'col-xs-12 col-sm-12 col-md-6 col-lg-6'
+    row.appendChild(col)
+
+    par = document.createElement('p')
+    col.appendChild(par)
+    par.className = 'lead tex2jax_proces'
+
+    title = entry.title.split(' (arXiv', 1)[0]
+    num = entry.link.split('/abs/')[1]
+    par.innerHTML = (
+      '<a href="https://arxiv.org/abs/' + num + '">' +
+      num +
+      '</a>' +
+      ' [<a href="https://arxiv.org/pdf/' + num + '">pdf</a>]' +
+      ': <span class="title">' +
+      title +
+      '</span>'
+    )
+
+    div = document.createElement('div')
+    col.appendChild(div)
+    div.className = 'authors'
+    div.innerHTML = htmlDecode(entry.author)
+
+    div = document.createElement('div')
+    col.appendChild(div)
+    div.className = 'abs tex2jax_process'
+    div.innerHTML = escapeHtml(entry.description.replace(/(<([^>]+)>)/gi, ''))
+  }
+}
+
+loadArXivJSON()
--- randomizer.php
+++ randomizer.php
@@ -1,214 +1,140 @@
-<!DOCTYPE html>
-<html>
-  <head lang="en" >
-    <meta charset="utf-8" >
-    <title>arXiv randomizer</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-    
-    <!-- Le styles -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" >
-    <link href="style.css" rel="stylesheet" >
-    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" >
-    <link href="style-responsive.css" rel="stylesheet" >
-    
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="bootstrap/js/html5shiv.js"></script>
-    <![endif]-->
-    
-    <!-- no mathjax
-    <script type="text/x-mathjax-config">
-     MathJax.Hub.Config({
-       tex2jax: {
-	 inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-	 processEscapes: true
-       }
-     });
-    </script>
-    -->
-  </head>
-  
-  <body class="tex2jax_ignore">    
-    <div class="container-fluid" >
-      <div class="page-header">
-	<h1>The ArXiv Randomizer: <small class="tagline">Less biased arXiv viewing.</small></h1>
-      </div>
-      
-      <?php
-      #get dates
-      date_default_timezone_set('America/New_York');
-      define('TIME_NOW', time());	
-      $day = intval(date('N', TIME_NOW)); # 1 for Monday, ..., 7 for Sunday
-      $hr = intval(date('H', TIME_NOW)) + intval(date('i', TIME_NOW))/60.0;
-      
-      $rss = simplexml_load_file("http://export.arxiv.org/rss/astro-ph");      
-      $time_rss = strtotime($rss->channel->children("http://purl.org/dc/elements/1.1/")->date);
-      
-      if(true) {
-	if($hr >= 20.5) {
-	  $today = true;
-	} else {
-	  $today = false;
-	}
-      } else {
-	if(date('j',$time_rss) == date('j',TIME_NOW)) {
-	  $today = true;
-	} else {
-	  $today = false;
-	}
-      }
-            
-      #arXiv "logic" for sending out new articles
-      #see this (http://arxiv.org/help/submit) and draw a picture
-      if($day == 7) {
-	#sunday
-	if ($today) {
-	  #night, so seeing stuff submitted from thurs 16:00 to fri 16:00
-	  $de = mktime(16-2*24,0,0);
-	  $ds = $de - 24.0*60.0*60.0;
-	} else {
-	  #day, so seeing stuff from wed 16:00 to thurs 16:00
-	  $de = mktime(16-3*24,0,0);
-	  $ds = $de - 24.0*60.0*60.0;
-	}
-      } elseif ($day == 1) {
-	#monday
-	if ($today) {
-	  #monday night, see stuff from fri 16:00 to mon 16:00
-	  $de = mktime(16,0,0);
-	  $ds = $de - 24.0*60.0*60.0*3;
-	} else {
-	  #monday day, seeing stuff from thurs 16:00 to fri 1600
-	  $de = mktime(16-3*24,0,0);
-	  $ds = $de - 24.0*60.0*60.0;
-	}
-      } elseif ($day == 2) {
-	#tuesday
-	if($today) {
-          #tuesday night is normal (monday 16 to tues 16)
-	  $de = mktime(16,0,0);
-	  $ds = $de - 24.0*60.0*60.0;
-        } else {
-	  #tues during day is fri 16 to mon 16
-          $de = mktime(-8,0,0);
-	  $ds = $de - 24.0*60.0*60.0*3;
-	}
-      } elseif ($day == 5) {
-	#friday - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
-	$de = mktime(16-24,0,0);
-	$ds = $de - 24.0*60.0*60.0;
-      } elseif($day == 6) {
-	#sat - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
-	$de = mktime(16-24*2,0,0);
-	$ds = $de - 24.0*60.0*60.0;
-      } else {
-	#all other days are "normal"
-	if ($today) {
-	  $de = mktime(16,0,0);
-	} else {
-	  $de = mktime(-8,0,0);
-	}
-	$ds = $de - 24.0*60.0*60.0;
-      }
-      
-      #construct the query
-      #$base_url = 'http://export.arxiv.org/api/query?search_query=';
-      #$query = $base_url . "cat:astro-ph*+AND+submittedDate:[".date('YmdH00',$ds)."+TO+".date('YmdH00',$de)."]" . "&start=0&max_results=100";
+    date_default_timezone_set('America/New_York');
+    define('TIME_NOW', time());
+    $day = intval(date('N', TIME_NOW)); // 1 for Monday, ..., 7 for Sunday
+    $hr = intval(date('H', TIME_NOW)) + intval(date('i', TIME_NOW))/60.0;
 
-      $items = $rss->item;            
-      $arr = array();
-      $np = 0;
-      foreach($items as $entry) {
-	if(strpos($entry->title,"UPDATED"))
-	  continue;
-	$arr[] = $entry;
-	$np = $np + 1;
-      }
-      $items = $arr;
-      shuffle($items);
-      
-      print("<div class=\"timestamp\">Showing ".$np." posts from ".date('D, j M Y H:i:s',$ds)." EDT to ".date('D, j M Y H:i:s',$de)." EDT.</div>\n");
-      #print("<hr>\n");
-      
-      $i = 0;
-      #output each entry
-      foreach($items as $entry) {
-	
-	if(strpos($entry->title,"UPDATED"))
-	  continue;
-	
-	if($i % 2 == 0) {
-	  print("<div class=\"row\">\n");
-	}
-	
-	print("<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-6\">\n");
-	
-	# split the id line to get just the arxiv id
-	$temp = split('/abs/',$entry->link);
-	$temp = substr($temp[1],0,strlen($temp[1]));
-	$title = $entry->title;
-	$pos = strpos($title," (arXiv");
-	$title = substr($title,0,$pos);
-	$title = htmlspecialchars($title,ENT_HTML5);
-	$hdr = "<p class=\"lead tex2jax_process\"><a href=\"http://arxiv.org/abs/".$temp."\">".$temp."</a>";
-	$hdr = $hdr . " [<a href=\"http://arxiv.org/pdf/".$temp.".pdf\">pdf</a>]";
-	print($hdr.": <span class=\"title\">".$title."</span></p>\n");
-	
-	# gather a list of authors
-	$auth = $entry->children("http://purl.org/dc/elements/1.1/");
-	$authors = $auth->creator;
-	print("<div class=\"authors\">".$authors."</div>\n"); 
-      
-	#abstract 
-	$abs = strip_tags($entry->description);
-	#$abs = htmlspecialchars($abs,ENT_HTML5);
-	print("<div class=\"abs tex2jax_process\">".$abs."</div>");
-	
-	print("</div>\n");
-	
-	if ($i % 2 == 1) {
-	  print("</div>\n");
-	}
-	
-	$i = $i + 1;
-      }
-            
-      if ($i % 2 == 1) {
-	print("</div>\n");
-      }
-      
-      ?>
-    </div>
-    
-    <!-- - Le javascript ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- <script type="text/javascript" src="mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script> -->
-    
-    <script type="text/javascript">
-      $(document).ready(function() {
-       $('.title').click(function() {
-	 if($(window).width() <= 768) {
-	   var $tgt = $(this).parent().nextAll('.abs:first');
-	   $tgt.slideToggle(200);
-	 }
-       });
-       
-       var $w = $(window).width();
-       $(window).resize(function() {
-	 if ($(window).width() > 768) {
-	   $('.abs').show();
-	 } else {
-	   if($w > 768) {
-	     $('.abs').hide();
-	   }
-	 }
-	 
-	 $w = $(window).width();
-       });
-      });
-    </script>
-    
-  </body>
-</html>
+    $rss = simplexml_load_file("http://export.arxiv.org/rss/astro-ph");
+    $time_rss = strtotime($rss->channel->children("http://purl.org/dc/elements/1.1/")->date);
+
+    if(true) {
+if($hr >= 20.5) {
+  $today = true;
+} else {
+  $today = false;
+}
+    } else {
+if(date('j',$time_rss) == date('j',TIME_NOW)) {
+  $today = true;
+} else {
+  $today = false;
+}
+    }
+
+    #arXiv "logic" for sending out new articles
+    #see this (http://arxiv.org/help/submit) and draw a picture
+    if($day == 7) {
+#sunday
+if ($today) {
+  #night, so seeing stuff submitted from thurs 16:00 to fri 16:00
+  $de = mktime(16-2*24,0,0);
+  $ds = $de - 24.0*60.0*60.0;
+} else {
+  #day, so seeing stuff from wed 16:00 to thurs 16:00
+  $de = mktime(16-3*24,0,0);
+  $ds = $de - 24.0*60.0*60.0;
+}
+    } elseif ($day == 1) {
+#monday
+if ($today) {
+  #monday night, see stuff from fri 16:00 to mon 16:00
+  $de = mktime(16,0,0);
+  $ds = $de - 24.0*60.0*60.0*3;
+} else {
+  #monday day, seeing stuff from thurs 16:00 to fri 1600
+  $de = mktime(16-3*24,0,0);
+  $ds = $de - 24.0*60.0*60.0;
+}
+    } elseif ($day == 2) {
+#tuesday
+if($today) {
+        #tuesday night is normal (monday 16 to tues 16)
+  $de = mktime(16,0,0);
+  $ds = $de - 24.0*60.0*60.0;
+      } else {
+  #tues during day is fri 16 to mon 16
+        $de = mktime(-8,0,0);
+  $ds = $de - 24.0*60.0*60.0*3;
+}
+    } elseif ($day == 5) {
+#friday - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
+$de = mktime(16-24,0,0);
+$ds = $de - 24.0*60.0*60.0;
+    } elseif($day == 6) {
+#sat - no new stuff - so always see thursday night stuff (so wed 16:00 to thurs 16:00)
+$de = mktime(16-24*2,0,0);
+$ds = $de - 24.0*60.0*60.0;
+    } else {
+#all other days are "normal"
+if ($today) {
+  $de = mktime(16,0,0);
+} else {
+  $de = mktime(-8,0,0);
+}
+$ds = $de - 24.0*60.0*60.0;
+    }
+
+    #construct the query
+    #$base_url = 'http://export.arxiv.org/api/query?search_query=';
+    #$query = $base_url . "cat:astro-ph*+AND+submittedDate:[".date('YmdH00',$ds)."+TO+".date('YmdH00',$de)."]" . "&start=0&max_results=100";
+
+    $items = $rss->item;
+    $arr = array();
+    $np = 0;
+    foreach($items as $entry) {
+if(strpos($entry->title,"UPDATED"))
+  continue;
+$arr[] = $entry;
+$np = $np + 1;
+    }
+    $items = $arr;
+    shuffle($items);
+
+    print("<div class=\"timestamp\">Showing ".$np." posts from ".date('D, j M Y H:i:s',$ds)." EDT to ".date('D, j M Y H:i:s',$de)." EDT.</div>\n");
+    #print("<hr>\n");
+
+    $i = 0;
+    #output each entry
+    foreach($items as $entry) {
+
+if(strpos($entry->title,"UPDATED"))
+  continue;
+
+if($i % 2 == 0) {
+  print("<div class=\"row\">\n");
+}
+
+print("<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-6\">\n");
+
+# split the id line to get just the arxiv id
+$temp = split('/abs/',$entry->link);
+$temp = substr($temp[1],0,strlen($temp[1]));
+$title = $entry->title;
+$pos = strpos($title," (arXiv");
+$title = substr($title,0,$pos);
+$title = htmlspecialchars($title,ENT_HTML5);
+$hdr = "<p class=\"lead tex2jax_process\"><a href=\"http://arxiv.org/abs/".$temp."\">".$temp."</a>";
+$hdr = $hdr . " [<a href=\"http://arxiv.org/pdf/".$temp.".pdf\">pdf</a>]";
+print($hdr.": <span class=\"title\">".$title."</span></p>\n");
+
+# gather a list of authors
+$auth = $entry->children("http://purl.org/dc/elements/1.1/");
+$authors = $auth->creator;
+print("<div class=\"authors\">".$authors."</div>\n");
+
+#abstract
+$abs = strip_tags($entry->description);
+#$abs = htmlspecialchars($abs,ENT_HTML5);
+print("<div class=\"abs tex2jax_process\">".$abs."</div>");
+
+print("</div>\n");
+
+if ($i % 2 == 1) {
+  print("</div>\n");
+}
+
+$i = $i + 1;
+    }
+
+    if ($i % 2 == 1) {
+print("</div>\n");
+    }
